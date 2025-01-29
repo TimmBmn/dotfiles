505559d03ff6cdcffdae8538ae462e38aee2e68e
Tim Baumann <TimmBmn@gmail.com>
2025-01-29 21:32:02 +0100

    feat: keybinds to navigate quickfix list

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -2,3 +2,7 @@ vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { silent = true })
 
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 vim.keymap.set({ "n", "v" }, "<leader>p", "\"+p")
+
+-- TODO add more keymaps to e.g. open/close quick fix list
+vim.keymap.set("n", "<C-j>", "<CMD>cnext<CR>")
+vim.keymap.set("n", "<C-k>", "<CMD>cprev<CR>")
